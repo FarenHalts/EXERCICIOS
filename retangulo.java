6d16ff4c6e758245491997950b8e6987d9cd1390
Lucas Garcia <47766322+FarenHalts@users.noreply.github.com>
2019-05-18 22:52:49 -0300

    Update retangulo.java

--- retangulo.java
+++ retangulo.java
@@ -1,3 +1,8 @@
+//Crie uma classe Rectangle com os atributos length e width, cada um dos quais assume o padrão de 1. Forneça os métodos 
+//que calculam o perímetro e a área do retângulo. A classe tem métodos set e get para o comprimento (length) e a largura (width). 
+//Os métodos set devem verificar se length e width são, cada um, números de ponto flutuante maiores que 0,0 e menores que 20,0. 
+//Escreva um programa para testar a classe Rectangle.
+
 import java.util.Scanner;
 public class Retangulo {
 	private float largura;
