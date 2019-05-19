22d14a0e1a3e29a4ac967d7ba04dbcc12a42f622
Lucas Garcia <47766322+FarenHalts@users.noreply.github.com>
2019-05-18 22:52:04 -0300

    Update Complex.java

--- Complex.java
+++ Complex.java
@@ -1,3 +1,14 @@
+//Crie uma classe chamada Complex para realizar aritmética com números complexos. Os números complexos têm a 
+//forma parteReal + parteImaginária * i, onde i é raíz quadrada de –1
+//Escreva um programa para testar sua classe. Utilize variáveis de ponto flutuante para representar os dados private 
+//da classe. Forneça um construtor que permita que um objeto dessa classe seja inicializado quando ele for declarado.
+//Forneça um construtor sem argumento com valores padrão caso nenhum inicializador seja fornecido. Forneça métodos public 
+//que realizam as seguintes operações:
+//a) Somar dois números Complex: as partes reais são somadas de um lado e as partes imaginárias, de outro.
+//b) Subtrair dois números Complex: a parte real do operando direito é subtraída da parte real do operando esquerdo e 
+//a parte imaginária do operando direito é subtraída da parte imaginária do operando esquerdo.
+//c) Imprima números Complex na forma (parteReal, parteImaginária).
+
 public class Complex {
     
     private double a;
